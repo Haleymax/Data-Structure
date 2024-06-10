7977a2f30a3b7bd0c08a5281ff4e5ca8ac657af4
haley <huanghongweimax@163.com>
2024-06-10 21:08:15 +0800

    配置文件

--- Data_Structure_Cpp/cmake-build-debug/cmake_install.cmake
+++ Data_Structure_Cpp/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/learn/ds/Data_Structure_Cpp
+# Install script for directory: E:/learn/ds/Data-Structure/Data_Structure_Cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/learn/ds/Data_Structure_Cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/learn/ds/Data-Structure/Data_Structure_Cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
