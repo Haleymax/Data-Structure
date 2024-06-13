a9549d97831b95d0bf8f1852b202dc4b57bb6b25
haley <huanghongweimax@163.com>
2024-06-13 23:59:29 +0800

    图实现的文件

---
+++ Data_Structure_Cpp/Graph/AL_graph.hpp
@@ -0,0 +1,11 @@
+//
+// Created by huang on 2024/6/13.
+//
+/**
+ * 基于邻接表实现的图
+ * **/
+
+#ifndef DATA_STRUCTURE_CPP_AL_GRAPH_H
+#define DATA_STRUCTURE_CPP_AL_GRAPH_H
+
+#endif //DATA_STRUCTURE_CPP_AL_GRAPH_H
---
+++ Data_Structure_Cpp/Graph/AM_graph.hpp
@@ -0,0 +1,13 @@
+//
+// Created by huang on 2024/6/13.
+//
+
+/**
+ * 基于邻接矩阵实现的图
+ * **/
+#ifndef DATA_STRUCTURE_CPP_AM_GRAPH_H
+#define DATA_STRUCTURE_CPP_AM_GRAPH_H
+
+#endif //DATA_STRUCTURE_CPP_AM_GRAPH_H
+
+
