b9e544e4c6c47ede899092026c775efd1c985ab4
haley <huanghongweimax@163.com>
2024-09-19 17:49:58 +0800

    python -array

--- Data_Structure_Python/Array/Array.py
+++ Data_Structure_Python/Array/Array.py
@@ -1,1 +1,21 @@
-class 
+class Array :
+    def __init__(self) :
+        self.array = []
+    def append(slef , value) :
+        slef.array.append(value)
+    def get(self , index) :
+        return self.array[index]
+    def len(self) :
+        return len(self.array)
+
+
+arr = Array()
+arr.append(1)
+arr.append(2)
+arr.append(3)
+arr.append(4)
+arr.append(5)
+
+print(arr.get(0))
+print(arr.get(1))
+print(arr.len())
